7bc76bf52713e940647520669af0be236ee7ee05
sssvt-anand <sssvtanand@gmail.com>
2025-06-20 16:36:39 +0530

    updated budgets

--- src/main/java/com/room/app/dto/MemberBudgetDetail.java
+++ src/main/java/com/room/app/dto/MemberBudgetDetail.java
@@ -1,21 +1,24 @@
 package com.room.app.dto;
 
-
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-
 public class MemberBudgetDetail {
-    private Long memberId;
-    private String memberName;
-    private BigDecimal monthlyBudget;
-    private BigDecimal usedBudget;
-    private BigDecimal remainingBudget;
-    private boolean overBudget;
-    private BigDecimal overBudgetAmount;
+	private Long memberId;
+	private String memberName;
+	private BigDecimal monthlyBudget;
+	private BigDecimal usedBudget;
+	private BigDecimal remainingBudget;
+	private boolean overBudget;
+	private BigDecimal overBudgetAmount;
+
+	public MemberBudgetDetail(Long memberId, String memberName, BigDecimal monthlyBudget, BigDecimal usedBudget,
+			BigDecimal remainingBudget, boolean overBudget, BigDecimal overBudgetAmount) {
+		this.memberId = memberId;
+		this.memberName = memberName;
+		this.monthlyBudget = monthlyBudget;
+		this.usedBudget = usedBudget;
+		this.remainingBudget = remainingBudget;
+		this.overBudget = overBudget;
+		this.overBudgetAmount = overBudgetAmount;
+	}
 }
